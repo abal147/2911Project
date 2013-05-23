9e9289b9c5d547cd1d38ab64ae63616fffbc5e17
unknown <dmloyzaga@gmail.com>
2013-05-23 14:09:53 +1000

    Updated the Solver, it though I don't think it's completely functional yet

--- src/Solver.java
+++ src/Solver.java
@@ -8,8 +8,10 @@ public class Solver {
 	/**
 	 * @param args
 	 */
-	public void sup (Board game){
+
+	public void assignSingleCell (Board game){
 		String cellString = new String();
+		Board gameBoard = game.clone();
 		for (int i=0; i<9; i++){
 			for (int j=0; j<9; j++){
 				cellString = game.getOptions(i, j);
@@ -18,6 +20,60 @@ public class Solver {
 				}
 			}
 		}
+		recursSolveBoard(gameBoard, 0, 0);
 	}
-	
+	/*
+	public void guessSingleCell(Board game){
+		String cellString = new String();
+		for (int i=0; i<9; i++){
+			for (int j=0; j<9; j++){
+				cellString = game.getOptions(i, j);
+				for (int k=0; k < cellString.length(); k++){
+					game.assign(i, j, cellString.charAt(k));
+				}
+			}
+		}
+	}
+
+	public Board solveBoard (Board game){
+		String cellString = new String();
+		Board gameBoard = game.clone();
+		for (int i=0; i <9; i++){
+			for (int j=0; j<9; j++){
+				cellString = game.getOptions(i,  j);
+				if (cellString.length() == 2){
+					gameBoard.assign(i, j, cellString.charAt(1));
+				}
+				else if (cellString.length() > 2){
+					for (int k=3; k<cellString.length(); k++){
+						game.assign(i, j, cellString.charAt(k));
+					}
+				}
+			}
+		}
+	}
+	*/
+	public void recursSolveBoard (Board game, int row, int col){
+		String cellString = new String();
+		Board gameBoard = game.clone();
+		
+				cellString = gameBoard.getOptions(row, col);
+		if (cellString.length() == 1){
+			if (col == 8){
+				recursSolveBoard (gameBoard, row+1, 0);
+			} else {
+				recursSolveBoard (gameBoard, row, col+1);
+			}
+
+		} else if (cellString.length() > 2) {
+			for (int i=0; i<cellString.length(); i++){
+				gameBoard.assign(row, col, cellString.charAt(i));
+				if (col == 8){
+					recursSolveBoard (gameBoard, row+1, 0);
+				} else {
+					recursSolveBoard (gameBoard, row, col+1);
+				}
+			}
+		}
+	}
 }
